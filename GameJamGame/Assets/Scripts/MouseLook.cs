ffb3a5f45315f31f45bd4378bd9fde017f9e4b17
Jasper Eyers <jaspereyers@hotmail.com>
2021-07-20 10:43:17 +1000

    added 3rd person camera
    of sorts

---
+++ GameJamGame/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //vars
+    [SerializeField] float rotationSpeed;
+    private float mouseX;
+    private float mouseY;
+
+    Transform target;
+
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -60, 40);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+
+}
