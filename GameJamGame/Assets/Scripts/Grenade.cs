a448804782fcfbb1f6231a563a94191a04810915
Luke <lukekov2002@gmail.com>
2021-07-20 17:47:06 +1000

    Grenade and other changes

---
+++ GameJamGame/Assets/Scripts/Grenade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private float timer;
+    [SerializeField] private GameObject explosion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
