8f93ea7067c05914c0cc204d7cf61ed22030ce06
James Webster <s200676@students.aie.edu.au>
2021-07-20 15:43:42 +1000

    New UI script

---
+++ GameJamGame/Assets/Scripts/GameUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public int score = 0;
+    public float timer = 30f;
+    public int survivorNumber;
+
+    public Text scoreText;
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        survivorNumber = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer - Time.deltaTime;
+
+        timer = (Mathf.Round(timer * 100) / 100);
+   
+        // When the level is beat
+        if (survivorNumber == 0)
+        {
+            SceneManager.LoadScene("EventHandler");
+
+            score++;
+
+            timer = 30;
+        }
+
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        timerText.text = "Time Left: " + timer;
+    }
+}
