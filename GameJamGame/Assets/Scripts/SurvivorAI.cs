ccb5e0d9a5c2d9311000f331c7681d89f42f7849
Luke <lukekov2002@gmail.com>
2021-07-20 14:32:15 +1000

    SurvivorAI Grenade Update
    Enemies now throw grenades on pressing G

--- GameJamGame/Assets/Scripts/SurvivorAI.cs
+++ GameJamGame/Assets/Scripts/SurvivorAI.cs
@@ -8,6 +8,10 @@ public class SurvivorAI : MonoBehaviour
     public GameObject survivor;
     public GameObject player;
 
+    // Grenade Variables
+    [SerializeField] private GameObject grenadePrefab;
+    [SerializeField] private float grenadeForce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,15 @@ public class SurvivorAI : MonoBehaviour
     void Update()
     {
         survivor.transform.LookAt(player.transform);
+        if (Input.GetKeyDown("g"))
+        {
+            GrenadeThrown();
+        }
+    }
+
+    void GrenadeThrown()
+    {
+        GameObject i = Instantiate(grenadePrefab, new Vector3(survivor.transform.position.x + 0.2f, survivor.transform.position.y + 1.2f, survivor.transform.position.z + 1f), survivor.transform.rotation);
+        i.GetComponent<Rigidbody>().AddForce(survivor.transform.forward * Random.Range(grenadeForce * 0.5f, grenadeForce) + new Vector3(0,Random.Range(5,11),0), ForceMode.Impulse);
     }
 }
