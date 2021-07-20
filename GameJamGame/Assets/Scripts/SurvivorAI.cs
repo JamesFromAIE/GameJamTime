656a7a2efdbddd62bf10036c106cc0aa56c29579
Luke <lukekov2002@gmail.com>
2021-07-20 12:55:25 +1000

    Survivor AI #1, Survivor enemy now looks at player
    Added survivor AI script that looks at the player, and attached it to the prefab. Also rotated survivor prefab as was backwards

---
+++ GameJamGame/Assets/Scripts/SurvivorAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivorAI : MonoBehaviour
+{
+    // Variables
+    public GameObject survivor;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        survivor = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        survivor.transform.LookAt(player.transform);
+    }
+}
