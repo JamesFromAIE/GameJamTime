0f82a6f5747552e4f8e85bd6625496fb48d8767c
Luke <lukekov2002@gmail.com>
2021-07-20 16:52:01 +1000

    Revert "Grenade Update"
    This reverts commit dda3cc6b15150cfb4967a9715c678f654482e8a9.

--- GameJamGame/Assets/Scripts/SurvivorAI.cs
+++ GameJamGame/Assets/Scripts/SurvivorAI.cs
@@ -7,21 +7,18 @@ public class SurvivorAI : MonoBehaviour
     // Variables
     public GameObject survivor;
     public GameObject player;
-    public GameObject explosion;
 
     // Grenade Variables
     [SerializeField] private GameObject grenadePrefab;
     [SerializeField] private float grenadeForce;
     [SerializeField] private float grenadeThrowTimer;
     [SerializeField] private float currentTimer;
-    private GameObject i;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         survivor = gameObject;
-        currentTimer = Random.Range(grenadeThrowTimer, grenadeThrowTimer * 2f);
     }
 
     // Update is called once per frame
@@ -34,19 +31,23 @@ public class SurvivorAI : MonoBehaviour
             GrenadeThrown();
             currentTimer = Random.Range(grenadeThrowTimer,grenadeThrowTimer * 1.5f);
         }
-        /*if (Input.GetKeyDown("g"))
+
+
+
+
+        if (Input.GetKeyDown("g"))
         {
             GrenadeThrown();
         }
         if (Input.GetKeyDown("b"))
         {
             DistanceBetween();
-        }*/
+        }
     }
 
     void GrenadeThrown()
     {
-        i = Instantiate(grenadePrefab, new Vector3(survivor.transform.position.x, survivor.transform.position.y + 2.7f, survivor.transform.position.z), Random.rotation);
+        GameObject i = Instantiate(grenadePrefab, new Vector3(survivor.transform.position.x, survivor.transform.position.y + 2.7f, survivor.transform.position.z), Random.rotation);
         float a = grenadeForce + DistanceBetween();
         i.GetComponent<Rigidbody>().AddForce(survivor.transform.forward * Random.Range(a * 0.75f, a * 1.25f) + new Vector3(0,Random.Range(5,11),0), ForceMode.Impulse);
     }
@@ -59,7 +60,7 @@ public class SurvivorAI : MonoBehaviour
         float d = survivor.transform.position.y;
         float e = player.transform.position.z;
         float f = survivor.transform.position.z;
-        //Debug.Log(Mathf.Abs(a - b + c - d + e - f));
+        Debug.Log(Mathf.Abs(a - b + c - d + e - f));
         return Mathf.Abs((a - b + c - d + e - f) / 3);
     }
 }
