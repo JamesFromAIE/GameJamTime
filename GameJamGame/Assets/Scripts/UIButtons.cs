e15398d8da80697d5bf2f15229a75353d68c06ba
James Webster <s200676@students.aie.edu.au>
2021-07-20 14:58:54 +1000

    Implemented Start Menu as a new scene

---
+++ GameJamGame/Assets/Scripts/UIButtons.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("AssetHandler");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
