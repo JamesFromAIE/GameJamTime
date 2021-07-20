c39a88920013e3f62c02f6e1c4ee01a5723e3bc6
Jasper Eyers <jaspereyers@hotmail.com>
2021-07-20 10:29:59 +1000

    added jump script
    and related scripts for proper use

---
+++ GameJamGame/Assets/Scripts/ChargeUp.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ChargeUp : MonoBehaviour
+{
+    [SerializeField] float maxTime = 3f;
+
+    float elapsedTime = 0f;
+    public float percentage = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (elapsedTime >= maxTime)
+            {
+                elapsedTime = maxTime;
+            }
+            else
+            {
+                elapsedTime += Time.deltaTime;
+            }
+        }
+
+        percentage = elapsedTime / maxTime;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            elapsedTime = 0;
+        }
+    }
+}
---
+++ GameJamGame/Assets/Scripts/PowerBar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour
+{
+    public Slider slider;
+    public ChargeUp chargeUp;
+
+    public void Update()
+    {
+        slider.value = chargeUp.percentage;
+    }
+}
